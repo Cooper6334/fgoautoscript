733f0d2385f6db48eb15a746ad368261c4151f3c
cooper6334 <cooper6334@gmail.com>
2021-09-09 03:22:49 +0800

    update debug html

--- other/debug_html.js
+++ other/debug_html.js
@@ -1,3 +1,12 @@
-class JavaScriptInterface{
-	
+var JavaScriptInterface = {
+	showMenu(){
+        console.log("show menu");
+    },
+    runScriptCallback (name,callback){
+        if(name==`initHTML("TW");`){
+            initHTML(`"";"";"";"";debug version`);
+        }
+    }
 }
+
+console.log("load html debug");
