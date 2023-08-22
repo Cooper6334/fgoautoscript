0847cd4117a7b04b5716220bbc5614001e97860f
Cooper Lin <cooper6334@gmail.com>
2023-08-23 00:57:31 +0900

    add test file

---
+++ debug/test.js
@@ -0,0 +1,14 @@
+console.log("test start");
+initScreenSize();
+isScriptRunning = true;
+isDebug = false;
+//=======================================
+
+saveCropIcon("teamAutoBuild");
+// console.log(icon["teamAutoBuild"]);
+// clickIcon("teamAutoBuild");
+// saveCropIcon("teamPage");
+
+//=======================================
+isScriptRunning = false;
+console.log("test finish");
